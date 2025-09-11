545905337c9648bd5082ca3d938d257fe4f996ff
Bini Babu <binibabu22@gmail.com>
2025-09-11 12:04:44 +0530

    Food Menu Next.js

---
+++ src/app/not-found.js
@@ -0,0 +1,8 @@
+export default function NotFoundpage() {
+  return (
+    <main className="not-found">
+      <h1>Meal not Found</h1>
+      <p>Unfortunately we cannot found the requested page or meal data.</p>
+    </main>
+  );
+}
